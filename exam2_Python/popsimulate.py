c53779f08a2d6273b58a70939f5cc0fdc576999f
gildatapuy-20 <gilda.tapuy@est.ikiam.edu.ec>
2023-07-01 16:09:07 -0500

    Ejercicio 1

--- exam2_Python/popsimulate.py
+++ exam2_Python/popsimulate.py
@@ -1,12 +1,13 @@
 #_________________ Función 1
+import numpy as np
 def build_population(N, p):
     population = []
     for i in range(N):
         allele1 = "A"
-        if scipy.random.rand() > p:
+        if np.random.rand() > p:
             allele1 = "a"
         allele2 = "A"
-        if scipy.random.rand() > p:
+        if np.random.rand() > p:
             allele2 = "a"
         population.append((allele1, allele2))
     return population
